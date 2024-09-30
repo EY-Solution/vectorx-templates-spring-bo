7641f9840ffaab1dbdc0610c8d55dc9d106bb693
RoyLee <keymasroy@gmail.com>
2024-09-30 18:14:03 +0900

    인증 커스톰 샘플 구현

--- src/main/java/me/vectorx/spring/templates/bo/config/AuthenticationConfiguration.java
+++ src/main/java/me/vectorx/spring/templates/bo/config/AuthenticationConfiguration.java
@@ -3,18 +3,24 @@ package me.vectorx.spring.templates.bo.config;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import org.springframework.cache.Cache;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseCookie;
 
 import me.vectorx.spring.management.bo.authentication.listener.VxManagementAuthenticationListener;
 import me.vectorx.spring.management.bo.authentication.processor.VxManagementBoJwtAuthenticationProcessor;
+import me.vectorx.spring.management.bo.authentication.user.VxCachedJwtManagementUserDetailProvider;
+import me.vectorx.spring.management.bo.authentication.user.VxManagmentUser;
 import me.vectorx.spring.management.properties.VxManagementBoProperties;
 import me.vectorx.spring.management.services.VxManagementRefreshTokenService;
 import me.vectorx.spring.mvc.utils.VxCookieUtils;
 import me.vectorx.spring.mvc.utils.VxWebUtils;
+import me.vectorx.spring.security.authentication.authentication.VxAuthentication;
+import me.vectorx.spring.security.authentication.cache.VxSecurityCacheSupport;
 import me.vectorx.spring.security.authentication.processor.VxAuthenticationProcessor;
 import me.vectorx.spring.security.authentication.request.token.VxAuthenticationRequestToken;
+import me.vectorx.spring.security.authentication.user.VxUser;
 import me.vectorx.spring.security.jwt.authentication.authentication.VxJwtAuthentication;
 import me.vectorx.spring.security.jwt.authentication.processor.VxJwtAuthenticationProcessor;
 
@@ -29,10 +35,37 @@ public class AuthenticationConfiguration {
 			VxManagementRefreshTokenService vxRefreshTokenService) {
 		return new VxManagementBoJwtAuthenticationProcessor(properties, vxRefreshTokenService, (processor) -> {
 			processor.setAuthenticationListener(new AuthenticationListener());
+			processor.setUserDetailProvider(new UserDetailProvider());
 		});
 	}
 	
+	/**
+	 * 사용자 정보 구현체
+	 */
+	public static class UserDetailProvider extends VxCachedJwtManagementUserDetailProvider<VxManagmentUser> {
+		
+		@Override
+		protected VxUser getInternalUserDetails(VxAuthenticationRequestToken authenticationRequestToken) {
+			// TODO 여기에 사용자 조회 로직 구현 
+			return super.getInternalUserDetails(authenticationRequestToken);
+		}
+		
+		@Override
+		protected VxUser getInternalUserDetails(VxJwtAuthentication authentication) {
+			// TODO 여기에 사용자 조회 로직 구현 
+			return super.getInternalUserDetails(authentication);
+		}
+		
+		@Override
+		protected Cache getCache() {
+			return VxSecurityCacheSupport.getDefaultUserCache();
+		}
+		
+	}
 	
+	/**
+	 * VxManagementAuthenticationListener 구현
+	 */
 	public static class AuthenticationListener extends VxManagementAuthenticationListener<VxAuthenticationRequestToken, VxJwtAuthentication> {
 		@Override
 		public void onAuthenticateSuccess(
